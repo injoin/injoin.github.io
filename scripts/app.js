be5578f86aab1d814788e1c8652706bc697e6c9a
Gustavo Henke <gustavo@injoin.io>
2015-03-13 00:11:38 -0300

    fix(post): make images responsive
    Closes #3

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 !function () {
     "use strict";
 
+    var $ = angular.element;
+
     angular.module( "app", [
         "ngMaterial"
     ], function ( $mdThemingProvider ) {
@@ -14,5 +16,22 @@
         $mdThemingProvider.theme( "twitter" ).primaryColor( "light-blue" );
 
         $mdThemingProvider.setDefaultTheme( "injoin" );
+    }).directive( "img", function () {
+        var dfn = {};
+
+        dfn.compile = function ( element ) {
+            var parent = element;
+            while ( parent.length ? parent = parent.parent() : null ) {
+                if ( parent.hasClass( "post-body" ) ) {
+                    return dfn.link;
+                }
+            }
+        };
+
+        dfn.link = function ( scope, element, attrs ) {
+            element.wrap( "<div class='img-container'></div>" );
+        };
+
+        return dfn;
     });
 }();
